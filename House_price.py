cf2f2c1ff80bdbb253d4f655c45f85a4207ce41e
Akter <mdkakter@gmail.com>
2019-11-16 23:24:59 +0000

    16/11/19

--- House_price.py
+++ House_price.py
@@ -4,23 +4,25 @@
 Created on Wed Oct 30 22:59:08 2019
 
 @author: smostafa
+pull dataset from URL
 """
 
 import os
 import tarfile
-from six.moves import urllib 
+from six.moves import urllib
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
 HOUSING_PATH = "datasets/housing"
 HOUSING_URL = DOWNLOAD_ROOT + HOUSING_PATH +"/housing.tgz"
 
 def fetch_housing_data(housing_url = HOUSING_URL, housing_path = HOUSING_PATH):
+    housing_path = r"/Users/smostafa/Desktop/ML_SKlearn/datasets/housing"
     if not os.path.exists(housing_path):
         os.makedirs(housing_path)
-    tgz_path = os.path.join(housing_path, "housing.tgz" ) 
+    tgz_path = os.path.join(housing_path, "housing.tgz" )
     urllib.request.urlretrieve(housing_url,tgz_path )
     housing_tgz = tarfile.open(tgz_path)
     housing_tgz.extractall(path = housing_path)
     housing_tgz.close()
 
-fetch_housing_data()    
+fetch_housing_data()
